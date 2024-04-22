9c1cb86f44c85d80268577d9751fbf9b4219647a
DavidTrilloEstech <david.trillo@escuelaestech.es>
2024-04-22 12:02:30 +0200

    get por actor, direcor y genero

--- backend/index.js
+++ backend/index.js
@@ -45,3 +45,34 @@ app.get('/series/:id' , async(req,res) => {
     const [result] = await pool.query('SELECT * FROM series where id = ?', [req.params.id])
     res.send(result)
 })
+ 
+app.get('/peliculas/genero/:id' , async(req,res) => {
+    const [result] = await pool.query('SELECT peliculas.nombre, peliculas.duracion, peliculas.valoracion, peliculas.rango_edad, peliculas.descripcion FROM categorias JOIN categorias_peliculas ON categorias.id = categorias_peliculas.id_categoria JOIN peliculas ON categorias_peliculas.id_pelicula = peliculas.id WHERE categorias.id = ? ', [req.params.id])
+    res.send(result)
+})
+
+app.get('/series/genero/:id' , async(req,res) => {
+    const [result] = await pool.query('SELECT series.nombre, series.rango_edad, series.num_temporadas, series.valoracion, series.descripcion  FROM categorias JOIN categorias_series ON categorias.id = categorias_series.id_categoria JOIN series ON categorias_series.id_serie = series.id WHERE categorias.id = ? ', [req.params.id])
+    res.send(result)
+})
+
+app.get('/peliculas/actor/:id' , async(req,res) => {
+    const [result] = await pool.query('SELECT peliculas.nombre, peliculas.duracion, peliculas.valoracion, peliculas.rango_edad, peliculas.descripcion FROM actores JOIN actores_peliculas ON actores.id = actores_peliculas.id_actor JOIN peliculas ON actores_peliculas.id_pelicula = peliculas.id WHERE actores.id = ? ', [req.params.id])
+    res.send(result)
+})
+
+app.get('/series/actor/:id' , async(req,res) => {
+    const [result] = await pool.query('SELECT series.nombre, series.rango_edad, series.num_temporadas, series.valoracion, series.descripcion FROM actores JOIN actores_series ON actores.id = actores_series.id_actor JOIN series ON actores_series.id_serie = series.id WHERE actores.id = ? ', [req.params.id])
+    res.send(result)
+})
+
+app.get('/peliculas/director/:id' , async(req,res) => {
+    const [result] = await pool.query('SELECT peliculas.nombre, peliculas.duracion, peliculas.valoracion, peliculas.rango_edad, peliculas.descripcion FROM directores JOIN peliculas ON directores.id = peliculas.id_director WHERE directores.id=? ', [req.params.id])
+    res.send(result)
+})
+
+app.get('/series/director/:id' , async(req,res) => {
+    const [result] = await pool.query('SELECT series.nombre, series.rango_edad, series.num_temporadas, series.valoracion, series.descripcion FROM directores JOIN serie_director ON directores.id = serie_director.id_director JOIN series ON serie_director.id_serie = series.id WHERE directores.id=? ', [req.params.id])
+    res.send(result)
+})
+
