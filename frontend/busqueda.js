9a18733ba9a2b671f585695fe283252b544e9451
Jose Manuel Borras Rodriguez <josemanuel.borras@esceuelaestech.es>
2024-06-03 09:58:22 +0200

    funciona el buscador

--- frontend/busqueda.js
+++ frontend/busqueda.js
@@ -1,15 +1,32 @@
+document.getElementById('search-input').addEventListener('input', buscar);
+
 async function buscar() {
-    const searchTerm = document.getElementById('searchTerm').value;
+    const searchTerm = document.getElementById('search-input').value;
+    const resultsContainer = document.getElementById('results-container');
+    const resultsDiv = document.getElementById('results-list');
+
+    if (searchTerm.length === 0) {
+        resultsContainer.style.display = 'none';
+        resultsDiv.innerHTML = '';
+        return;
+    }
+
     const response = await fetch(`http://localhost:3000/busqueda?searchTerm=${searchTerm}`);
     const data = await response.json();
-    const resultsDiv = document.getElementById('results');
     resultsDiv.innerHTML = '';
+
+    if (data.length > 0) {
+        resultsContainer.style.display = 'block';
+    } else {
+        resultsContainer.style.display = 'none';
+    }
 
     data.forEach(item => {
         const itemDiv = document.createElement('div');
+        itemDiv.className = 'result-item';
         itemDiv.innerHTML = `
-            <p>${item.nombre}</p>
-            <button onclick="agregarAFavoritos(${item.id}, '${item.tipo}')">Añadir a Favoritos</button>
+            <img src="${item.imagen}" alt="${item.nombre}">
+            <h3>${item.nombre}</h3>
         `;
         resultsDiv.appendChild(itemDiv);
     });
